8ee06d5dbdae7fd8823d0e1c94833b627adf331c
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2023-12-01 00:02:05 +0200

    up

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -2,7 +2,33 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
+      let input = document.querySelector("textarea").value;
+      let array = JSON.parse(input);
+      let obj = {};
+      console.log(array);
+      for (const restaurant of array) {
+         //console.log(restaurant);
+         let [restaurantName, workers] = restaurant.split(' - ');
+         //console.log(restaurantName + '->' + workers);
+         obj[restaurantName] = workers;
       
+      }
+      //console.log(obj);
+      let workersArr = [];
+      for (let rest in obj) {
+         console.log(rest);
+         let workersObj = {};
+         let workers = obj[rest].split(', ');
+         //console.log(workers);
+         for (let worker of workers) {
+            let [workerName, salary] = worker.split(' ');
+            console.log(workerName + '->' + salary);
+            workersObj[workerName] = salary;
+           
+         }
+          workersArr.push(workersObj);
+         //console.log(workersObj);
+      }
+      console.log(workersArr);
    }
 }
