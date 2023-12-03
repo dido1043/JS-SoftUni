25cbf07ef56c5a180f5eb2cced956a0dd1307e42
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2023-12-03 17:12:05 +0200

    T03.Locked profile

--- 03. Locked-Profile/app.js
+++ 03. Locked-Profile/app.js
@@ -1,3 +1,28 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.querySelectorAll("button"));
+    buttons.forEach((b) => {
+        b.addEventListener('click', showMore)
+    });
+
+    function showMore(event) {
+        let btn = event.target;
+        let profile = btn.parentElement;
+        let children = Array.from(profile.children);
+
+
+        let radioButtonUnlocked = children[4];
+        let info = children[9];
+        if (!radioButtonUnlocked.checked) {
+            return;
+        }
+
+        //toggle functions
+        if (btn.textContent === "Show more") {
+            info.style.display = 'block'
+            btn.textContent = 'Hide it'
+        } else if (btn.textContent === 'Hide it') {
+            info.style.display = 'none'
+            btn.textContent = 'Show more'
+        }
+    }
 }
