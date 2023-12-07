ed09ae0729e086914e8606269a175ce5493182a1
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2023-12-07 20:14:55 +0200

    T05. Encode-and-Decode-Messages

--- 05. Encode-and-Decode-Messages/app.js
+++ 05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,41 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let  textToEncode = document.getElementsByTagName('textarea')[0];
+    let testAreaForResult = document.getElementsByTagName('textarea')[1];
+
+    const encodeButton = document.querySelectorAll("button")[0];
+    const decodeButton = document.querySelectorAll("button")[1];
+    let txt = "";
+    
+    encodeButton.addEventListener('click', encode);
+    decodeButton.addEventListener('click', decode);
+    
+    
+    //encoding
+    function encode() {
+        let originalText = textToEncode.value;
+        txt = originalText;
+        let asciiCodeArray = [];
+        for (const letter of textToEncode.value) {
+            let toAscii = ascii(letter);
+            toAscii++;
+            asciiCodeArray.push(toAscii);
+        }
+        
+        let result = '';
+        for (const asciiNum of asciiCodeArray) {
+            result += String.fromCharCode(asciiNum);
+        }
+        textToEncode.textContent = "";
+        testAreaForResult.textContent = result;
+        
+    }
+    //decoding
+    function decode() {
+        if (testAreaForResult.value == "") {
+            return;
+        }
+        testAreaForResult.textContent = txt;
+    }
+
+    function ascii(a) { return a.charCodeAt(0); }
 }
