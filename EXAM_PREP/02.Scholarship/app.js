11d54e4618ac2b424fc8fa84ae848e41b753e52f
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2023-12-15 15:14:28 +0200

    up

--- EXAM_PREP/02.Scholarship/app.js
+++ EXAM_PREP/02.Scholarship/app.js
@@ -7,7 +7,11 @@ function solve() {
 
   const nextBtn = document.querySelector('#next-btn');
 
-  nextBtn.addEventListener('click', () =>{
+  nextBtn.addEventListener('click', () => {
+    const student = studentInput.value;
+    const uni = universityInput.value;
+    const score = scoreInput.value;
+
     let ul = document.querySelector('#preview-list');
     let li = document.createElement('li');
     li.className = "application";
@@ -15,13 +19,13 @@ function solve() {
     let article = document.createElement('article');
 
     let h4 = document.createElement('h4');
-    h4.textContent = studentInput.value;
+    h4.textContent = student;
 
     let universityParagraph = document.createElement('p');
-    universityParagraph.textContent = "University: " + universityInput.value;
+    universityParagraph.textContent = "University: " + uni;
 
     let scoreParagraph = document.createElement('p');
-    scoreParagraph.textContent = "Score: " + scoreInput.value;
+    scoreParagraph.textContent = "Score: " + score;
 
     let editButton = document.createElement('button');
     editButton.classList.add('action-btn');
@@ -31,19 +35,46 @@ function solve() {
     let applyButton = document.createElement('button');
     applyButton.classList.add('action-btn');
     applyButton.classList.add('apply');
-    applyButton.textContent = 'delete';
+    applyButton.textContent = 'apply';
 
-    console.log(editButton);
+    
     ul.appendChild(li);
+
     li.appendChild(article)
+
     article.appendChild(h4);
     article.appendChild(universityParagraph);
     article.appendChild(scoreParagraph);
+
     li.appendChild(editButton);
     li.appendChild(applyButton);
 
-    console.log(ul);
-    nextBtn.setAttribute('disabled', true)
-  });
 
+    nextBtn.setAttribute('disabled', true);
+
+    editButton.addEventListener('click', edit);
+    function edit() {
+      //Add this info to input form
+      studentInput.value = student;
+      universityInput.value = uni;
+      scoreInput.value = score;
+      //Delete from previw list
+      ul.removeChild(li);
+      //[Next] button must be enabled again.
+      nextBtn.removeAttribute('disabled');
+    }
+    applyButton.addEventListener('click', apply);
+    function apply() {
+      //task must be deleted from the <ul> with id "preview-list" 
+      ul.removeChild(li);
+      //task must be appended to the <ul> with id "candidates-list"
+      let candidatesList = document.querySelector('#candidates-list');
+      candidatesList.appendChild(li);
+      //The buttons [Edit] and [Apply] should be removed from the <li> element
+      li.removeChild(editButton);
+      li.removeChild(applyButton);
+      // [Next] button must be enabled again.
+      nextBtn.removeAttribute('disabled');
+    }
+  });
 }
